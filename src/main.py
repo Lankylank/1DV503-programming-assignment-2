6fe700d10745d74588d8c416b061a4deb89d429f
Lankylank <jl225qb@student.lnu.se>
2022-03-05 09:50:19 +0100

    removed unneccesary comments

--- src/main.py
+++ src/main.py
@@ -215,32 +215,6 @@ print("\nSELECT * FROM game WHERE title ='gta' Title is a good key here because 
 for i in result:
   print(i)
 
-#tableName = "realtion"
-#schema = "game_title CHAR(64), store CHAR(64), FOREIGN KEY (game_title) REFERENCES Game(game_title)"
-#schema = " (" + schema + ")"
-#DatabaseManager.cursor.execute("CREATE TABLE " + tableName + schema )
- 
-
-#schema = "game_title, store"
-#schema = " (" + schema + ")"
-#values = "'gta', 'steam'"
-#values = " VALUES(" + values + ")"
-#DatabaseManager.cursor.execute("INSERT INTO " + tableName + schema + values)
-#DatabaseManager.connector.commit()
-
-
-#schema = "game_title, store"
-#schema = " (" + schema + ")"
-#values = "'gta', 'ps4'"
-#values = " VALUES(" + values + ")"
-#DatabaseManager.cursor.execute("INSERT INTO " + tableName + schema + values)
-#DatabaseManager.connector.commit()
-
-
-#sql = "SELECT game_title FROM realtion WHERE game_title='gta'"
-#DatabaseManager.Execute(sql)
-#result = DatabaseManager.cursor.fetchall()
-
 
 
 #sql = "UPDATE Game SET year='1367' WHERE game_title='gta'"
