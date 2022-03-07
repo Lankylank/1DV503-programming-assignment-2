e742aac0ffbc1780d9155c686713641b3ec967d5
Lankylank <jl225qb@student.lnu.se>
2022-03-07 16:41:07 +0100

    queries

--- src/test1.py
+++ src/test1.py
@@ -12,6 +12,106 @@ DB_NAME   = "game_database"
 DatabaseManager = CDatabaseManager.CDatabaseManager(username, password, host)
 DatabaseManager.Connect() # Connect to to database and a crusor with this function
 DatabaseManager.SelectDatabase(DB_NAME)
+
+GAME_NAME = 'minecraft'
+
+# has to be used when creating a database, otherwise some queries wont work.
+# only has to be created once because views auto updates it valuse when the
+# normal tables updates.
+def create_priceStatistics_view():
+  sql = ("CREATE VIEW price_statistics AS "
+        "SELECT title, min(price) AS min, "
+        "max(price) AS max, "
+        "round(avg(price)) as avg "
+        "FROM title_game_store_table "
+        "GROUP BY title") ### if we dont have this, it only gets 1 row
+  DatabaseManager.Execute(sql)
+
+
+def getGameInfo(title: str):
+  sql = "SELECT * FROM title_table WHERE title = '" + title + "'" 
+  DatabaseManager.Execute(sql)
+  result = DatabaseManager.cursor.fetchall()
+  return result
+
+def getGameGenres(title: str):
+  sql = "SELECT genre FROM title_genre_table WHERE title = '" + title + "'" 
+  DatabaseManager.Execute(sql)
+  result = DatabaseManager.cursor.fetchall()
+  return result
+
+def getGamePlatforms(title: str):
+  sql = "SELECT platform FROM title_platform_table WHERE title = '" + title + "'" 
+  DatabaseManager.Execute(sql)
+  result = DatabaseManager.cursor.fetchall()
+  return result
+
+def getGamePrices(title: str):
+  sql = "SELECT game_store, price FROM title_game_store_table WHERE title = '" + title + "'" 
+  DatabaseManager.Execute(sql)
+  result = DatabaseManager.cursor.fetchall()
+  return result
+
+### GET ALL THE INFO ABOUT A SPECIFIC GAME ###  MAIN QUERY  ####
+def getAll(title: str):
+## we has to do this because the table we use, have multiple tuples with the same title
+## so we have to use GROUP_CONCAT which groups all of them together into a single row,
+## otherwise we would have multiple rows but only with different genre's. Added DISTINCT to this
+## because this removes duplicates otherwise we would have the same genre mentioned several times
+## because a game has several platforms, it prints all of them out, and if we dont use DISTINCT
+## we print the same thing over and over again for each of those specific platforms. We only
+## want a single record, of each uniqe information a game has.
+
+## GROUP_CONCAT returns a concatenated string, meaning it groups up all the different entrys
+## that a table has and adds them together into a single row. (dev.mysql.com/doc)
+  sql = ("SELECT title_table.*, "
+        "GROUP_CONCAT(DISTINCT(title_genre_table.genre) SEPARATOR ', '), "
+        "GROUP_CONCAT(DISTINCT(title_platform_table.platform) SEPARATOR ', '), "
+        "GROUP_CONCAT(DISTINCT(title_game_store_table.game_store) SEPARATOR ', '), "
+        "price_statistics.min, price_statistics.max, price_statistics.avg "
+        "FROM title_table "
+        "JOIN title_genre_table USING (title) "
+        "JOIN title_platform_table USING (title) "
+        "JOIN title_game_store_table USING (title) "
+        "JOIN price_statistics USING (title) "
+        "WHERE title_table.title = '" + title + "'"
+        "GROUP BY title_table.title")
+
+  DatabaseManager.Execute(sql)
+  result = DatabaseManager.cursor.fetchall()
+  return result
+
+
+## USING GROUP BY HERE only prduces 1 record, it skips all other entrys.
+'''
+sql = ("SELECT *"
+       "FROM title_table "
+       "JOIN title_genre_table USING (title) "
+       "WHERE title_table.title = 'minecraft'"
+       "GROUP BY title_table.title")
+DatabaseManager.Execute(sql)
+result = DatabaseManager.cursor.fetchall()
+for i in result:
+  print(i)
+'''
+
+### THIS ALSO WORKS #### but no joins, only subqueries
+## uses as motivation for our getAll() function.
+'''
+sql = ("SELECT title_table.*,( "
+       "SELECT GROUP_CONCAT(title_genre_table.genre) "
+       "FROM title_genre_table "
+       "WHERE title_table.title = title_genre_table.title)"
+       "FROM title_table "
+       "WHERE title_table.title = 'minecraft'")
+DatabaseManager.Execute(sql)
+result = DatabaseManager.cursor.fetchall()
+for i in result:
+  print(i)
+'''
+
+##########   CREATE DATABSE HERE   ##########################
+
 '''
 file = open('game_data_ordered.csv')
 game_data = csv.reader(file)
@@ -173,4 +273,3 @@ for i in range(0, numTables):
         DatabaseManager.connector.commit()
         values = str()
 '''
-
